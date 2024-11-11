bb3359390591906e7d987eebcad13cd81c8c3316
mahendra <pvdmahendra@gmail.com>
2024-11-11 18:19:11 +0530

    backend

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,35 @@
+import User from "../models/user.model";
+
+export const signup = (req,res) => {
+    try {
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+
+        if(password !== confirmPassword) {
+            return res.status(400).json({error:"Password not matched"});
+        }
+
+        const user = User.findOne({username});
+
+        if(user) {
+            return res.status(400).json({error: "Username already exists"})
+        }
+
+        // Hash password
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        
+
+    } catch (error) {
+        
+    }
+}
+
+export const login = (req,res) => {
+    res.send("login")
+}
+
+export const logout = (req,res) => {
+    res.send("logout")
+}
---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.routes.js"
+import connectDB from './db/connectToMongoDB.js'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+dotenv.config()
+
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+
+// app.get('/', (req,res) => {res.send("Hello")})
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running on PORT ${PORT}`);
+})
