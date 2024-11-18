5d4b7a5b5bd54f3f4cf91fdf06642a7089606396
mahendra <pvdmahendra@gmail.com>
2024-11-18 23:07:13 +0530

    created message model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -18,13 +18,13 @@ const userSchema = new mongoose.Schema({
     gender: {
         type: String,
         required: true,
-        enum: ["male", "female"]
+        enum: ["male", "female"]    
     },
     profilePic: {
         type: String,
         default: ""
     }
-});
+}, {timestamps: true});
 
 const User = mongoose.model("User", userSchema)
 
